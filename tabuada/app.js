2562743e4b865cacb468e4e69a09e26e63325d14
vinicius-ribas-machado <106859102+vinicius-ribas-machado@users.noreply.github.com>
2022-11-26 12:16:49 -0300

    Add files via upload

---
+++ tabuada/app.js
@@ -0,0 +1,28 @@
+function tabuada(){
+    let num = document.getElementById('value')
+    let tab = document.getElementById('seltab')
+    
+    if (num.value.length == 0){
+        window.alert('sem nenhum dado')
+    }
+    else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while( c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} X ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++ 
+            
+        }
+
+    }
+
+}
+
+
+
+
+
+
